0d65d68d47310c18b3196fc64144d3ac8d03d30b
BBarisKilic <bulentbariskilic@gmail.com>
2020-09-13 23:28:26 +0300

    Second Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,40 @@ namespace Square_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int length = 0;
+
+            while (length!=1)
+            {
+                Console.WriteLine();
+                Console.Write("Karenin birim uzunluğunu giriniz: ");
+                length = Convert.ToInt32(Console.ReadLine());
+
+                for (int column = 0; column < length; column++)
+                {
+                    for (int row = 0; row < length; row++)
+                    {
+                        if (column == 0 || column == length - 1)
+                        {
+                            if(column == length - 1 && row == 0){
+                                Console.WriteLine();
+                            }
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            if (row == 0 || row == length - 1)
+                            {
+                                if(row == 0){
+                                    Console.WriteLine();
+                                } 
+                                Console.Write("*");
+                            }else{
+                                Console.Write(" ");
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
